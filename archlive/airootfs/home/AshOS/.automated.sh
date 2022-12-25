51a8c0e53018980b216ba2cf9952085b8762ff61
Himanshu <himanshu312001@gmail.com>
2022-12-25 20:30:08 +0530

    chore: QOL changes

--- archlive/airootfs/home/AshOS/.automated.sh
+++ archlive/airootfs/home/AshOS/.automated.sh
@@ -8,12 +8,12 @@ gsettings set org.gnome.desktop.wm.preferences theme "Colloid-Yellow-Dark-Catppu
 
 gsettings set org.gnome.shell.extensions.user-theme name "Colloid-Yellow-Dark-Catppuccin-mocha"
 
-ln -sf /home/liveuser/.themes/Colloid-Yellow-Dark-Catppuccin-mocha/gtk-4.0/ /home/liveuser/.config/gtk-4.0
+ln -sf /home/AshOS/.themes/Colloid-Yellow-Dark-Catppuccin-mocha/gtk-4.0/ /home/AshOS/.config/gtk-4.0
 
 # gsettings set org.gnome.desktop.interface icon-theme "Catppuccin-mocha"
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/liveuser/.themes/wallpapers/forrest.png"
+gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/AshOS/.themes/wallpapers/forrest.png"
 
 
--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -23,6 +23,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/home/liveuser/.automated.sh"]="0:0:755"
+  ["/home/AshOS/.automated.sh"]="0:0:755"
   ["/root/move-sudoers.sh"]="0:0:755"
 )
